a2b16d9880fdb3a02aa1c9a09783bbb719b37daa
Mia Watanabe <mia.noriko.watanabe@gmail.com>
2022-12-14 15:55:07 -0800

    Added react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// react Bootstrap
+// https://react-bootstrap.github.io/getting-started/introduction/
+// The following line can be included in your src/index.js or App.js file
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
